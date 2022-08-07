72fcbcf619137cf1ef17a9facf87ea739c58cabd
Timothy-Adams <adamstimothyt@gmail.com>
2022-08-07 15:40:43 -0700

    added prompt, added alert

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 alert("Welcome Anime Fans");
 
+alert("What's your favorite anime?");
 
+let woohoo=prompt ("What would you rate my page? , 1-5");
+  for (let i=0; i<woohoo;i++) {
+    document.write("<img src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:AN…9mICe0vt6esI1XHyBeBfqA4f2LX8g6sNXCvHNniI&usqp=CAU'/>")
+
+  }
