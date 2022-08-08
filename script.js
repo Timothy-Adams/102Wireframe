affe4e7a58bf61ef7a988b7135b5f2e24e7c9c35
Timothy-Adams <adamstimothyt@gmail.com>
2022-08-07 17:48:23 -0700

    forgot to commit

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-alert("Welcome Anime Fans");
-
-alert("What's your favorite anime?");
-
-let woohoo=prompt ("What would you rate my page? , 1-5");
-  for (let i=0; i<woohoo;i++) {
+let woohoo=prompt ("Rate my page? , 1-5");
+  for (let i=0; i<woohoo;i++)
+  {
     document.write("<img src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:AN…9mICe0vt6esI1XHyBeBfqA4f2LX8g6sNXCvHNniI&usqp=CAU'/>")
-
-  }
+      
+}
