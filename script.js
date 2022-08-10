340442fe3894484ce4d588b8982f7292e348afb5
Timothy-Adams <adamstimothyt@gmail.com>
2022-08-09 18:47:34 -0700

    added dynamics JS to my website

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let woohoo=prompt ("Rate my page? , 1-5");
-  for (let i=0; i<woohoo;i++)
-  {
-    document.write("<img src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:AN…9mICe0vt6esI1XHyBeBfqA4f2LX8g6sNXCvHNniI&usqp=CAU'/>")
-      
+let woohoo = prompt("Rate my page? , 1-5");
+  for (let i = 0; i<woohoo;i++){
+    document.write("<img src='Star.png' class='loop' />");
+  }
+
+alert("Welcome Anime Fans!");
+
+let righton = prompt("Do you like anime?");
+if (righton == "yes"){
+  alert("Right on!");
+  document.write ("You are an anime lover! Welcome!");
+} else {
+  alert("Let me change your mind!");
+  document.write ("I hope I can change your mind about anime!");
 }
+
